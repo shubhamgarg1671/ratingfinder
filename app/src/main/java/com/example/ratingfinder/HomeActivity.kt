d379f63ebcb4b4d3aa5088c5124e556b5fc233d9
Shubham Garg <shubhamgarg1671@gmail.com>
2021-05-09 11:48:00 +0530

    Home Activity added

---
+++ app/src/main/java/com/example/ratingfinder/HomeActivity.kt
@@ -0,0 +1,34 @@
+package com.example.ratingfinder
+
+import android.app.ActivityManager
+import android.content.Intent
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        val button = findViewById<Button>(R.id.findRating)
+
+        // operations to be performed
+        // when user tap on the button
+        button.setOnClickListener()
+        {
+                    val intent = Intent(this, MainActivity::class.java).apply {
+
+                    }
+                    startActivity(intent)
+        }
+
+        // I had copied this code from stackoverflow
+        // Since volley is used for api requests and volley uses cache memory for same requests sent multiple times
+        // So I am trying to delete app deta here so that latest rating are fetched always.
+        this.cacheDir.deleteRecursively()
+
+    }
+}
